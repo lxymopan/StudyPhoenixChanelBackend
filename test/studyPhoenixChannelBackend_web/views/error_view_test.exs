f5eec183f2b8f0fa729ff73cb9a1923bd423effd
LiangXiuyu <Liang.Xiuyu@access-company.com>
2021-08-16 01:25:17 +0900

    inital project

---
+++ test/studyPhoenixChannelBackend_web/views/error_view_test.exs
@@ -0,0 +1,15 @@
+defmodule StudyPhoenixChannelBackendWeb.ErrorViewTest do
+  use StudyPhoenixChannelBackendWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(StudyPhoenixChannelBackendWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(StudyPhoenixChannelBackendWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
